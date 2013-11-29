eff432ab2715bbf0aeb936707d554d37cb682a0b
lo222fj <lo222fj@student.lnu.se>
2013-11-29 15:45:51 +0100

    Namnsortering och ålderberäkningar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,78 +4,61 @@ var makePerson = function(persArr) {
 	console.log(persArr);
 	var result = {};
 	// Din kod här...
-	
+
 	//Namnsträng, sorterad
-	console.log( typeof (persArr));
-
-	console.log(persArr[0].name);
-
 	var names = [];
 	var name;
 	var i;
 
-	//console.log(names + "före for");
 	for ( i = 0; i < persArr.length; i++) {
 		name = persArr[i].name;
 		names.push(name);
-		console.log(names);
 	}
 
+	names.sort(function(a, b) {
+		return a.localeCompare(b, "sv")
+	});
+	//"sv" gör att man vet att det blir svenska!
 
-	names.sort(function(a, b) { return a.localeCompare(b) });
-	console.log(names + "efter sort")
-	var namesString =names.join(", ");
-	console.log(namesString + "efter join");
-	
+	var namesString = names.join(", ");
+
 	//Åldrar
-	var birthdays = [];
-	var birthday;
-	var dateTypeBirthday = {};
-	var dateTypeBirthdays = [];
-			
+
+	var age;
+	var ages = [];
+	var maxAge;
+	var minAge;
+	var averageAge;
+
 	for ( i = 0; i < persArr.length; i++) {
 		console.log("i for birthday");
-		birthday = persArr[i].born;
-		console.log(birthday);
-		birthdays.push(birthday);
-		dateTypeBirthday = new Date((new Date(birthday)).setHours(0, 0, 0, 0))
-		//dateTypeBirthday = new Date(birthday);
-		//dateTypeBirthdays = push(dateTypeBirthday);
-		console.log(birthdays);
-		console.log(dateTypeBirthdays);
+		age = persArr[i].age;
+		console.log(age);
+		ages.push(age);
 	}
-	
-	
-	
-	
-	/*var minAge = function
-	
-	//Max
-Array.max = function( array ){
-    return Math.max.apply( Math, array );
-};
+	//Maxålder
+	Array.max = function(array) {
+		return Math.max.apply(Math, array);
+	};
+	maxAge = Array.max(ages);
 
+	//Minålder
+	Array.min = function(array) {
+		return Math.min.apply(Math, array);
+	};
+	minAge = Array.min(ages);
+	//Genomsnittsålder
+	averageAge = Math.round(ages.reduce(function(x, y) {
+		return x + y
+	}, 0) / ages.length);
+	console.log(averageAge);
 
-//function ( array ){
-    return Math.max.apply( Math, array );
-}
-Array.max(test);
-30
-
-
-//Min
-function ( array ){
-    return Math.min.apply( Math, array );
-}
-Array.min(test);
-10m
-	*/
-	
+	//Objekt att returnera
 	var objekt = {
 		names : namesString,
-		minAge : 0,
-		maxAge : 10,
-		averageAge : 5
+		minAge : minAge,
+		maxAge : maxAge,
+		averageAge : averageAge
 	};
 
 	return objekt;
