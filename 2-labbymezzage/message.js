94d6cc316e2daec003875540c27dffa1e1859d4e
lo222fj <lo222fj@student.lnu.se>
2013-12-06 09:42:34 +0100

    Moment 1-2
    Konstruktormetod och test

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,4 +1,32 @@
-function Message(){
-	
-	return this;
+function Message(message, date) {
+	console.log(message + date);
+	this.getText = function() {
+		console.log("this.getText " + message + date);
+		return message;
+	}
+	this.setText = function(_text) {
+		console.log("this.setText " + message + _text + date);
+		message = _text;
+	};
+	this.getDate = function() {
+		console.log("this.getDate " + message + date);
+		return date;
+	};
+	this.setDate = function(_date) {
+		console.log("this.setDate " + message + date + _date);
+		date = _date;
+	};
+
+	this.setText(message);
+	console.log("anrop av this.setText " + message + date);
+	this.setDate(date);
+	console.log("anrop av this.setDate " + message + " " + date);
+}
+
+Message.prototype.toString = function() {
+	//alert(this.date.getHours()+" "+this.date.getMinutes() +" "+this.date.getSeconds());
+	return this.getText() + " \(" + this.getDate() + " \)";
+}
+Message.getHTMLText = function() {
+	message = message.replace(/\n/g, '<br />');
 }
--- 2-labbymezzage/program.js
+++ 2-labbymezzage/program.js
@@ -1,10 +1,14 @@
-/*Efter detta ska du skapa ett statiskt objekt (objektliteral) i vilket du lägger själva applikationens 
-”motor”. Detta statiska objekt lägger du i en egen js-fil. 
-*/
-var motor ={};
-
-motor.init =function () {
-  alert("Jonas e unnnndeeerbaaaar!!!!");
+/*Efter detta ska du skapa ett statiskt objekt (objektliteral) i vilket du lägger själva applikationens
+ ”motor”. Detta statiska objekt lägger du i en egen js-fil.
+ */
+window.onload = function() {
+	motor.init();
+};
+var motor = {};
+motor.init = function() {
+	var newMessage = new Message("plupp", new Date());
+	alert(newMessage);
+	alert(newMessage.getText());
+	newMessage.setText("Jonas är en mupp");
+	alert(newMessage);
 }
-
-window.onload=function(){motor.init();};
