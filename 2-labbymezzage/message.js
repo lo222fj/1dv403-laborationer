4bd8fd6d02fbdfe881b3803d8739450e94ed3ddb
lo222fj <lo222fj@student.lnu.se>
2013-12-06 20:41:38 +0100

    Omarbetning
    Struktur, skapa objekt, kopplat event till knapp

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,26 +1,26 @@
 function Message(message, date) {
 	console.log(message + date);
 	this.getText = function() {
-		console.log("this.getText " + message + date);
+		//console.log("this.getText " + message + date);
 		return message;
-	}
+	};
 	this.setText = function(_text) {
-		console.log("this.setText " + message + _text + date);
+		//console.log("this.setText " + message + _text + date);
 		message = _text;
 	};
 	this.getDate = function() {
-		console.log("this.getDate " + message + date);
+		//console.log("this.getDate " + message + date);
 		return date;
 	};
 	this.setDate = function(_date) {
-		console.log("this.setDate " + message + date + _date);
+		//console.log("this.setDate " + message + date + _date);
 		date = _date;
 	};
 
 	this.setText(message);
-	console.log("anrop av this.setText " + message + date);
+	//console.log("anrop av this.setText " + message + date);
 	this.setDate(date);
-	console.log("anrop av this.setDate " + message + " " + date);
+	//console.log("anrop av this.setDate " + message + " " + date);
 }
 
 Message.prototype.toString = function() {
--- 2-labbymezzage/program.js
+++ 2-labbymezzage/program.js
@@ -1,14 +1,68 @@
 /*Efter detta ska du skapa ett statiskt objekt (objektliteral) i vilket du lägger själva applikationens
  ”motor”. Detta statiska objekt lägger du i en egen js-fil.
  */
-window.onload = function() {
-	motor.init();
-};
-var motor = {};
-motor.init = function() {
-	var newMessage = new Message("plupp", new Date());
-	alert(newMessage);
-	alert(newMessage.getText());
-	newMessage.setText("Jonas är en mupp");
-	alert(newMessage);
+var MessageBoard = {
+
+	messages : [],
+	renderMessages : function() {
+
+	},
+	init : function(e) {
+
+	},
+	createNewMessage : function() {
+		//var message = document.getElementById("saveMessage");
+
+		//var mess = new Message("hej hopp", new Date());
+		//this.messages.push(mess);
+		//var mess = new Message("HEJ HOPP", new Date());
+		//this.messages.push(mess);
+		//alert(MessageBoard.messages);
+		//alert(MessageBoard.messages[1].getText());
+	},
+	renderMessage : function() {
+
+	}, //skriver ut ett meddelande
+	renderMessages : function() {
+
+	},//loopar igenom meddelanden och anropar renderMessage för varje objekt
 }
+
+window.onload = function() {
+	debugger;
+	MessageBoard.init();
+	
+	var submit = document.getElementById("saveMessage");
+	
+	submit.onclick = function(){
+		var text;
+		var message={};
+		
+		text = document.getElementById("writeMessage").value;
+		console.log(text);
+		message = new Message(text, new Date());
+		console.log(message);
+		MessageBoard.messages.push(message);
+		console.log("FÖRE");
+		console.log(MessageBoard.messages);
+		console.log("EFTER");
+		
+	};
+	
+	
+	//alert(text);
+
+};
+
+/*
+ window.onload = function() {
+ motor.init();
+ };
+ var motor = {};
+ motor.init = function() {
+ var newMessage = new Message("plupp", new Date());
+ alert(newMessage);
+ alert(newMessage.getText());
+ newMessage.setText("Jonas är en mupp");
+ alert(newMessage);
+ }*/
